c9a72c7b42a0278e10887147ab1a596a57724879
Christopher Hunt <christopherhunt@christohersmbp2.lan>
2025-10-08 17:24:48 +0200

    Fix TypeScript errors in Confetti class
    - Converted minified Confetti code to TypeScript compatible version
    - Added proper type annotations for all functions and parameters
    - Fixed 'this' context issues that were causing build failures
    - Maintained all confetti functionality and animation features

--- app/page.tsx
+++ app/page.tsx
@@ -10,8 +10,201 @@ import Image from "next/image";
 import Link from "next/link";
 import BetterWheel, { BetterWheelRef } from "@/components/BetterWheel";
 
-// Confetti class
-var Confetti=function(){var t=function(){return function(){this.gravity=10,this.particle_count=75,this.particle_size=1,this.explosion_power=25,this.destroy_target=!0,this.fade=!1}}(),e=function(){function e(n){var r=this;if(this.bursts=[],this.setCount=function(t){if("number"!=typeof t)throw new Error("Input must be of type 'number'");e.CONFIG.particle_count=t},this.setPower=function(t){if("number"!=typeof t)throw new Error("Input must be of type 'number'");e.CONFIG.explosion_power=t},this.setSize=function(t){if("number"!=typeof t)throw new Error("Input must be of type 'number'");e.CONFIG.particle_size=t},this.setFade=function(t){if("boolean"!=typeof t)throw new Error("Input must be of type 'boolean'");e.CONFIG.fade=t},this.destroyTarget=function(t){if("boolean"!=typeof t)throw new Error("Input must be of type 'boolean'");e.CONFIG.destroy_target=t},this.setupCanvasContext=function(){if(!e.CTX){var t=document.createElement("canvas");e.CTX=t.getContext("2d"),t.width=2*window.innerWidth,t.height=2*window.innerHeight,t.style.position="fixed",t.style.top="0",t.style.left="0",t.style.width="calc(100%)",t.style.height="calc(100%)",t.style.margin="0",t.style.padding="0",t.style.zIndex="999999999",t.style.pointerEvents="none",document.body.appendChild(t),window.addEventListener("resize",function(){t.width=2*window.innerWidth,t.height=2*window.innerHeight})}},this.setupElement=function(t){var n;r.element=document.getElementById(t),null===(n=r.element)||void 0===n||n.addEventListener("click",function(t){var n=new o(2*t.clientX,2*t.clientY);r.bursts.push(new i(n)),e.CONFIG.destroy_target&&(r.element.style.visibility="hidden")})},this.update=function(t){r.delta_time=(t-r.time)/1e3,r.time=t;for(var e=r.bursts.length-1;e>=0;e--)r.bursts[e].update(r.delta_time),0==r.bursts[e].particles.length&&r.bursts.splice(e,1);r.draw(),window.requestAnimationFrame(r.update)},!n)throw new Error("Missing id");e.CONFIG||(e.CONFIG=new t),this.time=(new Date).getTime(),this.delta_time=0,this.setupCanvasContext(),this.setupElement(n),window.requestAnimationFrame(this.update)}return e.prototype.draw=function(){s.clearScreen();for(var t=0,e=this.bursts;t<e.length;t++){e[t].draw()}},e}(),i=function(){function t(t){this.particles=[];for(var i=0;i<e.CONFIG.particle_count;i++)this.particles.push(new n(t))}return t.prototype.update=function(t){for(var e=this.particles.length-1;e>=0;e--)this.particles[e].update(t),this.particles[e].checkBounds()&&this.particles.splice(e,1)},t.prototype.draw=function(){for(var t=this.particles.length-1;t>=0;t--)this.particles[t].draw()},t}(),n=function(){function t(t){this.size=new o((16*Math.random()+4)*e.CONFIG.particle_size,(4*Math.random()+4)*e.CONFIG.particle_size),this.position=new o(t.x-this.size.x/2,t.y-this.size.y/2),this.velocity=r.generateVelocity(),this.rotation=360*Math.random(),this.rotation_speed=10*(Math.random()-.5),this.hue=360*Math.random(),this.opacity=100,this.lifetime=Math.random()+.25}return t.prototype.update=function(t){this.velocity.y+=e.CONFIG.gravity*(this.size.y/(10*e.CONFIG.particle_size))*t,this.velocity.x+=25*(Math.random()-.5)*t,this.velocity.y*=.98,this.velocity.x*=.98,this.position.x+=this.velocity.x,this.position.y+=this.velocity.y,this.rotation+=this.rotation_speed,e.CONFIG.fade&&(this.opacity-=this.lifetime)},t.prototype.checkBounds=function(){return this.position.y-2*this.size.x>2*window.innerHeight},t.prototype.draw=function(){s.drawRectangle(this.position,this.size,this.rotation,this.hue,this.opacity)},t}(),o=function(){return function(t,e){this.x=t||0,this.y=e||0}}(),r=function(){function t(){}return t.generateVelocity=function(){var t=Math.random()-.5,i=Math.random()-.7,n=Math.sqrt(t*t+i*i);return i/=n,new o((t/=n)*(Math.random()*e.CONFIG.explosion_power),i*(Math.random()*e.CONFIG.explosion_power))},t}(),s=function(){function t(){}return t.clearScreen=function(){e.CTX&&e.CTX.clearRect(0,0,2*window.innerWidth,2*window.innerHeight)},t.drawRectangle=function(t,i,n,o,r){e.CTX&&(e.CTX.save(),e.CTX.beginPath(),e.CTX.translate(t.x+i.x/2,t.y+i.y/2),e.CTX.rotate(n*Math.PI/180),e.CTX.rect(-i.x/2,-i.y/2,i.x,i.y),e.CTX.fillStyle="hsla("+o+"deg, 90%, 65%, "+r+"%)",e.CTX.fill(),e.CTX.restore())},t}();return e}();
+// Confetti class - TypeScript compatible
+declare global {
+  interface Window {
+    Confetti: any;
+  }
+}
+
+const ConfettiClass = function() {
+  var Config = function() {
+    return function() {
+      this.gravity = 10;
+      this.particle_count = 75;
+      this.particle_size = 1;
+      this.explosion_power = 25;
+      this.destroy_target = true;
+      this.fade = false;
+    };
+  }();
+  
+  var ConfettiMain = function(id: string) {
+    this.bursts = [];
+    this.element = null;
+    this.time = (new Date()).getTime();
+    this.delta_time = 0;
+    
+    if (!id) throw new Error("Missing id");
+    if (!ConfettiMain.CONFIG) {
+      ConfettiMain.CONFIG = new Config();
+    }
+    
+    this.setupCanvasContext();
+    this.setupElement(id);
+    window.requestAnimationFrame(this.update.bind(this));
+  };
+  
+  ConfettiMain.prototype.setupCanvasContext = function() {
+    if (!ConfettiMain.CTX) {
+      var canvas = document.createElement("canvas");
+      ConfettiMain.CTX = canvas.getContext("2d");
+      canvas.width = 2 * window.innerWidth;
+      canvas.height = 2 * window.innerHeight;
+      canvas.style.position = "fixed";
+      canvas.style.top = "0";
+      canvas.style.left = "0";
+      canvas.style.width = "calc(100%)";
+      canvas.style.height = "calc(100%)";
+      canvas.style.margin = "0";
+      canvas.style.padding = "0";
+      canvas.style.zIndex = "999999999";
+      canvas.style.pointerEvents = "none";
+      document.body.appendChild(canvas);
+      window.addEventListener("resize", function() {
+        canvas.width = 2 * window.innerWidth;
+        canvas.height = 2 * window.innerHeight;
+      });
+    }
+  };
+  
+  ConfettiMain.prototype.setupElement = function(id: string) {
+    var self = this;
+    this.element = document.getElementById(id);
+    if (this.element) {
+      this.element.addEventListener("click", function(event: MouseEvent) {
+        var position = new Vector(2 * event.clientX, 2 * event.clientY);
+        self.bursts.push(new Burst(position));
+        if (ConfettiMain.CONFIG.destroy_target) {
+          self.element.style.visibility = "hidden";
+        }
+      });
+    }
+  };
+  
+  ConfettiMain.prototype.update = function(timestamp: number) {
+    this.delta_time = (timestamp - this.time) / 1000;
+    this.time = timestamp;
+    
+    for (var i = this.bursts.length - 1; i >= 0; i--) {
+      this.bursts[i].update(this.delta_time);
+      if (this.bursts[i].particles.length === 0) {
+        this.bursts.splice(i, 1);
+      }
+    }
+    
+    this.draw();
+    window.requestAnimationFrame(this.update.bind(this));
+  };
+  
+  ConfettiMain.prototype.draw = function() {
+    Utils.clearScreen();
+    for (var i = 0; i < this.bursts.length; i++) {
+      this.bursts[i].draw();
+    }
+  };
+  
+  var Burst = function(position: Vector) {
+    this.particles = [];
+    for (var i = 0; i < ConfettiMain.CONFIG.particle_count; i++) {
+      this.particles.push(new Particle(position));
+    }
+  };
+  
+  Burst.prototype.update = function(deltaTime: number) {
+    for (var i = this.particles.length - 1; i >= 0; i--) {
+      this.particles[i].update(deltaTime);
+      if (this.particles[i].checkBounds()) {
+        this.particles.splice(i, 1);
+      }
+    }
+  };
+  
+  Burst.prototype.draw = function() {
+    for (var i = this.particles.length - 1; i >= 0; i--) {
+      this.particles[i].draw();
+    }
+  };
+  
+  var Particle = function(position: Vector) {
+    this.size = new Vector(
+      (16 * Math.random() + 4) * ConfettiMain.CONFIG.particle_size,
+      (4 * Math.random() + 4) * ConfettiMain.CONFIG.particle_size
+    );
+    this.position = new Vector(
+      position.x - this.size.x / 2,
+      position.y - this.size.y / 2
+    );
+    this.velocity = VelocityGenerator.generateVelocity();
+    this.rotation = 360 * Math.random();
+    this.rotation_speed = 10 * (Math.random() - 0.5);
+    this.hue = 360 * Math.random();
+    this.opacity = 100;
+    this.lifetime = Math.random() + 0.25;
+  };
+  
+  Particle.prototype.update = function(deltaTime: number) {
+    this.velocity.y += ConfettiMain.CONFIG.gravity * (this.size.y / (10 * ConfettiMain.CONFIG.particle_size)) * deltaTime;
+    this.velocity.x += 25 * (Math.random() - 0.5) * deltaTime;
+    this.velocity.y *= 0.98;
+    this.velocity.x *= 0.98;
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+    this.rotation += this.rotation_speed;
+    if (ConfettiMain.CONFIG.fade) {
+      this.opacity -= this.lifetime;
+    }
+  };
+  
+  Particle.prototype.checkBounds = function() {
+    return this.position.y - 2 * this.size.x > 2 * window.innerHeight;
+  };
+  
+  Particle.prototype.draw = function() {
+    Utils.drawRectangle(this.position, this.size, this.rotation, this.hue, this.opacity);
+  };
+  
+  var Vector = function(x: number, y: number) {
+    this.x = x || 0;
+    this.y = y || 0;
+  };
+  
+  var VelocityGenerator = function() {};
+  VelocityGenerator.generateVelocity = function() {
+    var x = Math.random() - 0.5;
+    var y = Math.random() - 0.7;
+    var length = Math.sqrt(x * x + y * y);
+    y /= length;
+    return new Vector(
+      (x / length) * (Math.random() * ConfettiMain.CONFIG.explosion_power),
+      y * (Math.random() * ConfettiMain.CONFIG.explosion_power)
+    );
+  };
+  
+  var Utils = function() {};
+  Utils.clearScreen = function() {
+    if (ConfettiMain.CTX) {
+      ConfettiMain.CTX.clearRect(0, 0, 2 * window.innerWidth, 2 * window.innerHeight);
+    }
+  };
+  Utils.drawRectangle = function(position: Vector, size: Vector, rotation: number, hue: number, opacity: number) {
+    if (ConfettiMain.CTX) {
+      ConfettiMain.CTX.save();
+      ConfettiMain.CTX.beginPath();
+      ConfettiMain.CTX.translate(position.x + size.x / 2, position.y + size.y / 2);
+      ConfettiMain.CTX.rotate(rotation * Math.PI / 180);
+      ConfettiMain.CTX.rect(-size.x / 2, -size.y / 2, size.x, size.y);
+      ConfettiMain.CTX.fillStyle = "hsla(" + hue + "deg, 90%, 65%, " + opacity + "%)";
+      ConfettiMain.CTX.fill();
+      ConfettiMain.CTX.restore();
+    }
+  };
+  
+  return ConfettiMain;
+}();
+
+// Make Confetti available globally
+(window as any).Confetti = ConfettiClass;
 
 export default function Page() {
   useViewportHeight();
@@ -3169,7 +3362,7 @@ export default function Page() {
             document.body.appendChild(confettiTarget);
             
             // Initialize confetti with the target
-            const confetti = new Confetti('confetti-target');
+            const confetti = new ConfettiClass('confetti-target');
             confetti.setCount(75);
             confetti.setSize(1);
             confetti.setPower(25);
